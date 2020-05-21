c0075aa892700a8dcbb8819adaa6a3aa9c1151ea
Ethan Hathaway <erhathaway@gmail.com>
2020-05-21 16:48:48 -0700

    Feat: add a bunch of analytic sources

--- app/index.tsx
+++ app/index.tsx
@@ -2,4 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import RootLayout from './layouts';
 
+import LogRocket from 'logrocket';
+LogRocket.init('personal-aftuk/httpserhathawaygithubio');
+
+import setupLogRocketReact from 'logrocket-react';
+
+// after calling LogRocket.init()
+setupLogRocketReact(LogRocket);
+
+LogRocket.getSessionURL(function(sessionURL) {
+    window.mixpanel.track('LogRocket', {sessionURL: sessionURL});
+});
+
+LogRocket.getSessionURL(function(sessionURL) {
+    window.heap.track('LogRocket', {sessionURL: sessionURL});
+});
+
+LogRocket.getSessionURL(function(sessionURL) {
+    window.ga('send', {
+        hitType: 'event',
+        eventCategory: 'LogRocket',
+        eventAction: sessionURL
+    });
+});
+
 ReactDOM.render(<RootLayout />, document.getElementById('root'));
